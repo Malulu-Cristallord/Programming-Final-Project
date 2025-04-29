dc9d100f3198f3315430a5953147dd0bb50ac0b4
Malulu-Cristallord <malucristallord@gmail.com>
2025-04-29 14:15:48 +0800

    Update Main.java

--- Final Project/src/Main.java
+++ Final Project/src/Main.java
@@ -4,6 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		for(int i = 0; i < 20; i++) {
+			System.out.println("What is this");
+		}
 	}
 
 }
