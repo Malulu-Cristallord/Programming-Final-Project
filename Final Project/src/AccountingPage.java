d076453872069859a37a0d2b716ce69a40a4f39f
Malulu-Cristallord <malucristallord@gmail.com>
2025-05-07 09:18:04 +0800

    None

--- Final Project/src/AccountingPage.java
+++ Final Project/src/AccountingPage.java
@@ -1,4 +1,17 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
 
 public class AccountingPage {
 
+	
+	public AccountingPage() {
+		createLayout();
+	}
+	
+	private void createLayout() {
+		
+	}
 }
--- Final Project/src/CalanderPage.java
+++ Final Project/src/CalanderPage.java
@@ -1,3 +1,8 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import java.io.*;
 
 public class CalanderPage {
 
